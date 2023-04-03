656e4b4e5d6ffe7c5b1da9f138208f37a10042e6
Ghabi Anis <73026191+ghabianis@users.noreply.github.com>
2023-04-03 14:06:50 +0100

    Update dbmlToJson.js

--- dbmlToJson.js
+++ dbmlToJson.js
@@ -6,6 +6,7 @@ sampleData = [];
 sheetNames = [];
 fields = []
 data_sw = [];
+relations = []
 let ws;
 
 fs.readFile("database.dbml", "utf8", (error, data) => {
@@ -17,7 +18,6 @@ fs.readFile("database.dbml", "utf8", (error, data) => {
         dataJson = Parser.parseDBMLToJSON(data);
 
         const wb = XLSX.utils.book_new();
-
         // Parsing the tables names and the fields 
         Object.keys(dataJson.tables).forEach(function (key, index1) {
             sheetNames.push(dataJson.tables[key].name);
@@ -25,11 +25,94 @@ fs.readFile("database.dbml", "utf8", (error, data) => {
 
         });
 
+        Object.keys(dataJson.refs).forEach(function (key, index1) {
+            relations.push(dataJson.refs[key].endpoints);
+        });
+
         //changing any user table to test table because devfacory already has a user table so it's not allowed to have two tables
         // with the same name
-        for (let j = 0; j < sheetNames.length; j++) {
-            if (sheetNames[j] == "User") {
-                sheetNames[j] = "Test";
+        // for (let j = 0; j < sheetNames.length; j++) {
+        //     if (sheetNames[j] == "User") {
+        //         sheetNames[j] = "Test";
+        //     }
+        // }
+
+        relationsDataSet = []
+
+        function createTableRelationsArray(tableRelations) {
+            const tableRelationObjects = [];
+            tableRelations.forEach((relation) => {
+                const primaryTable = {
+                    schemaName: relation[0].schemaName,
+                    tableName: relation[0].tableName,
+                    fieldNames: relation[0].fieldNames[0],
+                    schemaName2: relation[1].schemaName,
+                    tableName2: relation[1].tableName,
+                    fieldNames2: relation[1].fieldNames[0],
+                };
+
+                relationsDataSet.push({ 'PrimaryTable': relation[0].tableName, 'SecondaryTable': relation[1].tableName, "field": relation[1].fieldNames[0], "relation": relation[0].relation[0] === "1" ? "one-to-many" : "many-to-one" })
+                const relationType = relation[0].relation === "1" ? "one-to-many" : "many-to-one";
+
+                tableRelationObjects.push({
+                    primaryTable,
+                    relationType,
+                });
+            });
+
+
+            return tableRelationObjects;
+        }
+
+        createTableRelationsArray(relations);
+
+        /// Create an empty object to store the tables
+        const tab = {};
+
+        // Loop through the array of objects
+        for (const obj of relationsDataSet) {
+            // Check if the primary table is already in the tab object
+            if (!tab[obj.PrimaryTable]) {
+                // If not, create a new object for the primary table
+                tab[obj.PrimaryTable] = {
+                    name: 'tab',
+                    tableName: obj.PrimaryTable,
+                    fields: [],
+                    relations: []
+                };
+            }
+
+            // Check if the secondary table is already in the tab object
+            if (!tab[obj.SecondaryTable]) {
+                // If not, create a new object for the secondary table
+                tab[obj.SecondaryTable] = {
+                    name: 'tab',
+                    tableName: obj.SecondaryTable,
+                    fields: [],
+                    relations: []
+                };
+            }
+
+            // Add the field to the primary table object
+            tab[obj.PrimaryTable].fields.push(obj.field);
+
+            // Determine the type of relationship
+            if (obj.relation === 'one-to-many') {
+                // If it's a one-to-many relationship, add it to the primary table object
+                tab[obj.PrimaryTable].relations.push({
+                    type: 'one-to-many',
+                    table: 'tab',
+                    tableName: obj.SecondaryTable,
+                    field: obj.field
+                });
+            } else {
+                // If it's a many-to-one relationship, add it to the secondary table object
+                tab[obj.SecondaryTable].relations.push({
+                    type: 'many-to-one',
+                    table: 'tab',
+                    tableName: obj.PrimaryTable,
+                    field: obj.field
+                });
             }
         }
 
@@ -57,6 +140,7 @@ fs.readFile("database.dbml", "utf8", (error, data) => {
     }
 });
 
+const cellsVlaues = []
 // the purpose of this function is to delete the first row and decremet the rest of the rows by 1
 function deleteFirstRowOfExcelSheets(filePath) {
     const workbook = XLSX.readFile(filePath);
@@ -71,9 +155,11 @@ function deleteFirstRowOfExcelSheets(filePath) {
             for (let C = range.s.c; C <= range.e.c; ++C) {
                 const cellRef = XLSX.utils.encode_cell({ r: R, c: C });
                 const cell = worksheet[cellRef];
-                if (cell && cell.t !== 'z') { // skip cells with type `z`, which are placeholders
+                // console.log(cell.v)
+                if (cell && cell.t !== 'z' ) { // skip cells with type `z`, which are placeholders
                     const newRef = XLSX.utils.encode_cell({ r: R - 1, c: C });
                     delete worksheet[cellRef];
+
                     cell.r = R - 1;
                     worksheet[newRef] = cell;
                 }
@@ -85,7 +171,86 @@ function deleteFirstRowOfExcelSheets(filePath) {
             e: { r: range.e.r - 1, c: range.e.c }
         });
 
+
+
     });
+
+    // Get the first sheet of the workbook
+    sheetNames.forEach((sheetName) => {
+        const worksheet = workbook.Sheets[sheetName];
+
+        // Get the range of the worksheet
+        const range = XLSX.utils.decode_range(worksheet['!ref']);
+
+        // Get the last row index
+        const lastRowIndex = range.e.r;
+
+        // Increment the last row index by 1 to get the row where you want to insert the new rows
+        const newRowStartIndex = lastRowIndex + 1;
+        let newRows = []
+        for (let index = 0; index < relationsDataSet.length; index++) {
+            const element = relationsDataSet[index];
+            if (sheetName == element.PrimaryTable) {
+                // Create an array of arrays, where each inner array contains the values you want to insert in the cells of the new rows
+                newRows.push( [
+                    element.SecondaryTable.replace(/(?:^|\s)\S/g, function(a) { return a.toUpperCase(); }),
+                     element.SecondaryTable.replace(/(?:^|\s)\S/g, function(a) { return a.toUpperCase(); })
+                ],)
+            }
+
+        }
+
+        // Add the new rows to the worksheet
+        XLSX.utils.sheet_add_aoa(worksheet, newRows, { origin: `${newRowStartIndex}:A` });
+
+        
+    })
+
+    sheetNames.forEach(sheetName => {
+        const worksheet = workbook.Sheets[sheetName];
+        const data = XLSX.utils.sheet_to_json(worksheet, {header:1});
+      
+        const filteredData = data.filter(row => {
+          return !(row.includes("createdAt") || row.includes("deletedAt") || row.includes("id") || row.includes("updatedAt"));
+        });
+      
+        const newWorksheet = XLSX.utils.json_to_sheet(filteredData);
+        workbook.Sheets[sheetName] = newWorksheet;
+
+        
+      });
+      sheetNames.forEach(sheetName => {
+        const worksheet = workbook.Sheets[sheetName];
+        const range = XLSX.utils.decode_range(worksheet['!ref']);
+        range.s.r += 1;
+        worksheet['!ref'] = XLSX.utils.encode_range(range);
+
+        for (let R = range.s.r; R <= range.e.r; ++R) {
+            for (let C = range.s.c; C <= range.e.c; ++C) {
+                const cellRef = XLSX.utils.encode_cell({ r: R, c: C });
+                const cell = worksheet[cellRef];
+                // console.log(cell.v)
+                if (cell && cell.t !== 'z' ) { // skip cells with type `z`, which are placeholders
+                    const newRef = XLSX.utils.encode_cell({ r: R - 1, c: C });
+                    delete worksheet[cellRef];
+
+                    cell.r = R - 1;
+                    worksheet[newRef] = cell;
+                }
+            }
+        }
+        // update sheet range to exclude first row
+        worksheet['!ref'] = XLSX.utils.encode_range({
+            s: { r: range.s.r - 1, c: range.s.c },
+            e: { r: range.e.r - 1, c: range.e.c }
+        });
+
+
+
+    });
+
+      
     XLSX.writeFile(workbook, filePath);
 }
+
 
